11e7659ec6120ce02882cd7c17ea99d8d2b738af
JustinCresswell <jcress1996@gmail.com>
2024-02-24 23:09:26 -0700

    Made it work
    everything outputs correctly now

--- prototype_UVSIM.py
+++ prototype_UVSIM.py
@@ -3,7 +3,6 @@ from math import e
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
-
 class UVSimulator:
     def __init__(self):
         # Initialize UVSim components
@@ -22,47 +21,48 @@ class UVSimulator:
         except Exception as e:
             return False
 
-    def verify_input(self):
-        #Verify the user enters a digit - reject all other inputs.
-        isValid = False
-        user_entry = ""
-        
-        while(isValid == False):
-            
-            user_entry = (input("Enter a value: "))
-            
+    def verify_input(self, entry):
+        #Verify the user enters a 4 digit value- reject all other inputs.
+        if len(entry) == 4:
             try:
-                user_entry = int(user_entry)
-                
+                int(entry)
             except Exception as e:
-                print("Invalid input. Please enter a numerical value.")
-                
-            else:
-                isValid = True
-                
-        return user_entry
-                
+                return False
+            return True
+        return False
+        
+simulator = UVSimulator()
+
 class ProgramController:
     def __init__(self, simulator):
         self.simulator = simulator
+        self.output = [] #used to write output to GUI
+        self.read_control = False #used to stop program to grab read input from gui
+        self.value = 0 #used to store read input from gui
+        self.done = True #used to stop gui from outputting prematurely
 
     def execute_program(self, max_iterations = 100):
     # Execute BasicML program
         while (self.simulator.instruction_counter < max_iterations):
             instruction = self.simulator.memory[self.simulator.instruction_counter]
             if instruction == -99999:
-                return "End of file."
+                return "\nEnd of file."
                 
-
             opcode = instruction // 100
             operand = instruction % 100
 
             if opcode == 10:  # READ
-                value = self.simulator.verify_input()
-                self.simulator.memory[operand] = value
-
+                #exits the loop so the user can input in the read field
+                if self.read_control is True:
+                    self.simulator.memory[operand] = self.value
+                    self.read_control = False
+                    self.done = True
+                else:
+                    self.done = False
+                    return "\nPlease input command in Read Field"
+                    
             elif opcode == 11:  # WRITE
-                print("Output:", self.simulator.memory[operand])
+                self.output.append(self.simulator.memory[operand])
 
             elif opcode == 20:  # LOAD
                 self.simulator.accumulator = self.simulator.memory[operand]
@@ -80,7 +80,7 @@ class ProgramController:
                 if self.simulator.memory[operand] != 0:
                     self.simulator.accumulator //= self.simulator.memory[operand]
                 else:
-                    return "Error: Division by zero"
+                    return "\nError: Division by zero"
                 
 
             elif opcode == 33:  # MULTIPLY
@@ -101,44 +101,78 @@ class ProgramController:
                     continue
 
             elif opcode == 43:  # HALT
-                return "Program halted."
+                return "\nProgram halted."
                 
             
             else:
                 #Invalid opcode
-                return (f"Invalid opcode: {opcode} is not a valid command.\nExiting program.")
+                return (f"\nInvalid opcode: {opcode} is not a valid command.\nExiting program.")
                 
-
             self.simulator.instruction_counter += 1
 
-simulator = UVSimulator()
+
+
 control = ProgramController(simulator)
 
 class MainGridLayout(Widget):
-    file= ObjectProperty(None)
+    file = ObjectProperty(None)
     read = ObjectProperty(None)
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.file = ""
+        self.read = ""
+        self.input_notification = False
+
     def press_file(self):
+        '''this method activates when the run button is clicked'''
+        self.file = self.ids.file.text #this grabs the data from the field
 
-        file = self.ids.file.text
+        file_input = simulator.load_program_from_file(self.file)
+        control.read_control=False
+        control.done= True
+        simulator.instruction_counter = 0
 
-        file_input = simulator.load_program_from_file(file)
         if file_input is True:
-            control.execute_program()
+            #checks fileinput
+            result = control.execute_program()
+            if control.done is True:
+                self.ids.output.text += str(result) + '\n' + str(simulator.memory)
+                self.ids.accumulator.text = str(simulator.accumulator)
+                self.ids.write.text = str(control.output)
+                self.ids.file.text = ''
 
-            self.ids.output.text= f'Output: {simulator.memory}'
-            self.ids.file.text = ''
+            else:
+                self.ids.output.text += str(result)
         else:
-            self.ids.output.text = "File Not Found"
+            self.ids.output.text = "\nFile Not Found"
             self.ids.file.text = ''
 
     def press_read(self):
-        read = self.read.text
+        #when Gui submit button is pressed, input is verified and execution continues from where it left off
+        self.read = self.ids.read.text
+        input_verification = simulator.verify_input(self.read) 
+        if input_verification is True:
+            control.value = int(self.read)
+            control.read_control = True
+            verified = control.execute_program()
+            if control.done is True:
+                if control.output:
+                    self.ids.output.text += str(control.output)
+                    print("accessed control.output")
+                self.ids.output.text += str(verified) + '\n' + str(simulator.memory)
+                self.ids.accumulator.text = str(simulator.accumulator)
+                self.ids.write.text = str(control.output)
+                self.ids.file.text = ''
 
-        print(f'read: {read}')
+            else:
+                self.ids.output.text += str(verified)
+            #right now, I'm having trouble making it continue where it left off
+        else:
+            self.ids.output.text += "\nInput Invalid. Try Again"
+            return
 
-        self.ids.read.text= ""
-
+        
 
 class SimApp(App):
     def build(self):
@@ -146,9 +180,6 @@ class SimApp(App):
 
 def main():
     SimApp().run()
-    # mysim = UVSimulator()
-    # mysim.load_program_from_file("Test2.txt")
-    # mysim.execute_program()
 
 if __name__=="__main__":
     main()
